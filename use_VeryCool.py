30733e2ad71ba6855e5c5f161794b2e923fca302
dataman-git <55301043+dataman-git@users.noreply.github.com>
2021-02-19 15:15:36 -0500

    use_VeryCool
    use_VeryCool

---
+++ use_VeryCool.py
@@ -0,0 +1,11 @@
+#
+import VeryCool
+
+greatCookie = VeryCool.cookie(20,'Frosting')
+
+	# smallCookie
+print("My great cookie is a " + greatCookie.theFlavor + " cookie.")
+print("It's area is: ")
+print(greatCookie.area())
+print("And it's perimeter is: ")
+print(greatCookie.perimeter())

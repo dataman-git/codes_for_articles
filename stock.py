2c6c392bab1f23d46c70c27c6bba9bf3a0e06398
dataman-git <55301043+dataman-git@users.noreply.github.com>
2021-06-03 21:21:15 -0400

    Update stock.py
    Add the "download csv file" section

--- stock.py
+++ stock.py
@@ -72,7 +72,31 @@ st.write('Recent data ')
 st.dataframe(df.tail(10))
 
 
+################
+# Download csv #
+################
 
+import base64
+from io import BytesIO
+
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    val = to_excel(df)
+    b64 = base64.b64encode(val)  # val looks like b'...'
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="download.xlsx">Download excel file</a>' # decode b'abc' => abc
+
+st.markdown(get_table_download_link(df), unsafe_allow_html=True)
 
 
 
